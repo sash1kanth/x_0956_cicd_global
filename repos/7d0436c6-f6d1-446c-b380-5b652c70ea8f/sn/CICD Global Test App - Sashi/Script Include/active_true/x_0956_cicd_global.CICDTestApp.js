7e01da5b9e23da789f56cd06b7b040aacfe10b43
System Administrator <admin@example.com>
2019-04-09 16:21:41 -0700

    Changes by 'System Administrator' on 'CICD Global Test App - Sashi – 1.0.0' -- Build #1

---
+++ repos/7d0436c6-f6d1-446c-b380-5b652c70ea8f/sn/CICD Global Test App - Sashi/Script Include/active_true/x_0956_cicd_global.CICDTestApp.js
@@ -0,0 +1,20 @@
+/* 
+ * Application : CICD Global Test App - Sashi
+ * ClassName   : sys_script_include
+ * Created On  : 2019-03-31 05:55:57
+ * Created By  : admin
+ * Updated On  : 2019-03-31 16:41:26
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=e86c1b01db6433002fc1e04a489619bf
+ */
+var CICDTestApp = Class.create();
+CICDTestApp.prototype = {
+    initialize: function() {
+    },
+	
+	test_app: function() {
+		gs.print("Testing app >>>> ");
+	},
+
+    type: 'CICDTestApp'
+};
